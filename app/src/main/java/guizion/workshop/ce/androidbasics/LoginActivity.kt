a9361bbd2342f33f7add473d229b39d97ca71c7d
Rafagan Abreu <rafagan@guizion.com>
2018-03-23 13:42:47 -0300

    Campos de senha, TextView clicável, princípios de navegação

--- app/src/main/java/guizion/workshop/ce/androidbasics/LoginActivity.kt
+++ app/src/main/java/guizion/workshop/ce/androidbasics/LoginActivity.kt
@@ -2,10 +2,18 @@ package guizion.workshop.ce.androidbasics
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.view.View
+
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun navigateToForgotPassword(view: View) {
+        val myIntent = Intent(this, PasswordRecoveryActivity::class.java)
+        startActivity(myIntent)
     }
 }
