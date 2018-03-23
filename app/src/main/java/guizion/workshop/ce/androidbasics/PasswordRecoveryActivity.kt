d3d2c4b6731536df2857bb28fe90372b75aaff5e
Rafagan Abreu <rafagan@guizion.com>
2018-03-23 15:17:28 -0300

    Recuperação de senha e backstack

--- app/src/main/java/guizion/workshop/ce/androidbasics/PasswordRecoveryActivity.kt
+++ app/src/main/java/guizion/workshop/ce/androidbasics/PasswordRecoveryActivity.kt
@@ -1,11 +1,24 @@
 package guizion.workshop.ce.androidbasics
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class PasswordRecoveryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password_recovery)
+    }
+
+    fun recoverPassword(view: View) {
+        //todo: efetuar recuperação de senha no servidor
+        this.navigateToLogin()
+    }
+
+    fun navigateToLogin() {
+        val myIntent = Intent(this, LoginActivity::class.java)
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(myIntent)
     }
 }
